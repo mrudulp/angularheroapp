ee7423132e37aa8cc03b06b04903614982991094
MVP <mrudulpen@hotmail.com>
2017-08-09 07:03:29 +0300

    Step 6 in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,14 @@
-import { Component } from '@angular/core';
-
-export class Hero{
-  id: number;
-  name: string;
-}
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'my-app',
-  // template: `<h1>{{title}}</h1><h2>{{hero.name}} details </h2>`,
-  template: `
-      <h1>{{title}}</h1>
-      <h2>{{hero.name}} details </h2>
-      <div><label>id: </label>{{hero.id}} </div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>`
+    selector:'my-app',
+    template:`
+        <h1> {{title}} </h1>
+        <a routerLink='/heroes'>Heroes </a>
+        <router-outlet></router-outlet>
+    `,
+    styles:[``]
 })
-export class AppComponent  { 
-  name = 'Angular';
+export class AppComponent{
   title = 'Tour of Heros';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
 }
